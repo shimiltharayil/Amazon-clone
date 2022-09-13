8ada1aa3ea3db422983577a243c1cbfa20b3442a
Shimil Tharayil <shimiltharayil007@gmail.com>
2022-09-13 19:36:39 +0530

    Banner Completed

---
+++ components/ProductFeed.tsx
@@ -0,0 +1,21 @@
+import Product from "./Product"
+function ProductFeed({products}) {
+    return (
+        <div className="grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:-mt-52">
+            {products.map(({ id, title , price, description, category, image}) => (
+          <Product
+           key={id}
+           id={id}
+           title={title}
+           price={price}
+           description={description}
+           category={category}
+           image={image}
+          
+          />
+            ))}
+            </div>
+                )
+            }
+
+export default ProductFeed;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import Header from '../components/Header'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+import ProductFeed from "../components/ProductFeed";
 
-const Home: NextPage = () => {
+ export default function Home ({products}) {
   return (
-    <div >
+    <div className="bg-gray-100">
       <Head>
         <title>Amazon</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-<Header />
+
+      <Header />
+      <main className="max-w-screen-2xl mx-auto">
+        {/* Banner */}
+       < Banner />
+
+        {/* Product Feed */}
+        <ProductFeed products = { products} />
+      </main>
     </div>
-  )
+  );
+};
+
+export async function getServerSideProps (context) {
+  const products = await fetch("https://fakestoreapi.com/products").then(
+    (res) => res.json()
+  );
+  return {
+    props: {
+      products,
+    }
+  }
 }
 
-export default Home
+
+// https://fakestoreapi.com/products
